4041767748b1e32d678a3665c7ba9ffaf8af7c04
Bruce Williams <brwcodes@gmail.com>
2016-01-30 17:57:50 -0800

    Work on list coercion checks, scalar list bug

--- lib/absinthe/execution/resolution/list.ex
+++ lib/absinthe/execution/resolution/list.ex
@@ -6,10 +6,11 @@ defimpl Absinthe.Execution.Resolution, for: Absinthe.Type.List do
     %{values: values_to_return, execution: execution_to_return} = target
     |> Enum.reduce(%{values: [], execution: execution}, fn (value_to_resolve, %{values: values_before, execution: current_execution}) ->
       this_resolution = %Resolution{type: wrapped_type, target: value_to_resolve}
-      {:ok, result, next_execution} = Resolution.resolve(
-        selection_set,
-        %{current_execution | resolution: this_resolution}
-      )
+      if selection_set do
+        {:ok, result, next_execution} = Resolution.resolve(
+          selection_set,
+          %{current_execution | resolution: this_resolution}
+        )
       %{values: [result|values_before], execution: next_execution}
     end)
     {:ok, values_to_return, execution_to_return}
--- test/specification/type_system/types/lists_test.exs
+++ test/specification/type_system/types/lists_test.exs
@@ -1,7 +1,63 @@
 defmodule Specification.TypeSystem.Types.ListsTest do
   use ExSpec, async: true
-  @moduletag specification: true, pending: true
+  @moduletag specification: true
 
   @graphql_spec "#sec-Lists"
+
+  defmodule Schema do
+    use Absinthe.Schema
+    alias Absinthe.Type
+
+    def query do
+      %Type.Object{
+        fields: fields(
+          valid: [
+            type: list_of(:integer),
+            resolve: fn _, _ -> {:ok, [1,2,3]} end
+          ],
+          invalid: [
+            type: list_of(:integer),
+            resolve: fn _, _ -> {:ok, [1]} end
+          ],
+          takes_list: [
+            type: :boolean,
+            args: args(
+              list: [type: list_of(:integer)]
+            ),
+            resolve: fn
+            %{list: val} when is_list(val)
+              -> {:ok, true}
+            _ -> {:ok, false}
+            end
+          ]
+        )
+      }
+    end
+
+  end
+
+  describe "if a list is returned" do
+    @query "{ valid }"
+    @tag :pending
+    describe "coercion passes" do
+      assert {:ok, %{data: %{"valid" => [1, 2, 3]}}} == Absinthe.run(@query, Schema)
+    end
+  end
+
+
+  describe "if a non-list is returned" do
+    @query "{ invalid }"
+    @tag :pending
+    describe "coercion fails" do
+      assert {:ok, %{errors: _}} = Absinthe.run(@query, Schema)
+    end
+  end
+
+  describe "if the value is passed as input to a list type is not a list" do
+    @tag :pending
+    it "should be coerced as though the input was a list of size one" do
+
+    end
+  end
 
 end
